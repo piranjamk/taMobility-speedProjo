e0f466213b189c508ffb96d30d3869feb173703b
piranjamk <piranjamk@gmail.com>
2022-05-15 20:50:17 +0200

    add LogsLoader class without logic

---
+++ taMobilitySpeedProjo/src/main/java/org/example/LogsLoader.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogsLoader {
+
+    public static List<LogRecord> LoadLogs(String logsFileName)
+    {
+        List<LogRecord> logsList = new ArrayList<>();
+
+        return logsList;
+    }
+}
